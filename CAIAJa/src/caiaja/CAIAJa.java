24c619c1d40367bcfafee6345e9b20084e4a268b
Fernando Engelhardt <fernando@engelsoft.com.br>
2016-06-30 16:37:42 -0400

    Inicialização do sistema jade

--- CAIAJa/src/caiaja/CAIAJa.java
+++ CAIAJa/src/caiaja/CAIAJa.java
@@ -5,6 +5,18 @@
  */
 package caiaja;
 
+import jade.Boot;
+import static jade.Boot.DEFAULT_FILENAME;
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.util.ExtendedProperties;
+import jade.util.leap.Properties;
+import jade.wrapper.AgentContainer;
+import jade.wrapper.AgentController;
+import jade.wrapper.StaleProxyException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author fosa
@@ -15,7 +27,36 @@ public class CAIAJa {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        AgentContainer ac = null;
+
+        ProfileImpl p = null;
+        Properties props = new ExtendedProperties();
+        p = new ProfileImpl(props);
+        props.setProperty(Profile.GUI, "true");
+
+        jade.core.Runtime.instance().setCloseVM(true);
+        if (p.getBooleanProperty(Profile.MAIN, true)) {
+            ac = jade.core.Runtime.instance().createMainContainer(p);
+        } else {
+            jade.core.Runtime.instance().createAgentContainer(p);
+        }
+
+        /**
+         * Inicializando Agentes
+         */
+        try {
+            AgentController Controlador1 = ac.createNewAgent("Controlador1", "caiaja.agentes.ControladorAgent", new Object[0]);
+            AgentController Controlador2 = ac.createNewAgent("Controlador2", "caiaja.agentes.ControladorAgent", new Object[0]);
+
+            Controlador1.start();
+            Controlador2.start();
+
+            AgentController Estacao = ac.createNewAgent("Estacao", "caiaja.agentes.EstacaoMeteorologicaAgent", new Object[0]);
+            Estacao.start();
+
+        } catch (StaleProxyException ex) {
+            Logger.getLogger(CAIAJa.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
+
 }
