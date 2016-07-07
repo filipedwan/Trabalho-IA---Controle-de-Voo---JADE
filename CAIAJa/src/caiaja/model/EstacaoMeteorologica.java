f2e63cbfed8299d86c87a2e628301b188f3f99e7
Lucia <Lucia@Lucia-PC.paricarana.ufrr.br>
2016-07-07 17:23:33 -0300

    Add atritutos basicos.

--- CAIAJa/src/caiaja/model/EstacaoMeteorologica.java
+++ CAIAJa/src/caiaja/model/EstacaoMeteorologica.java
@@ -13,5 +13,41 @@ import java.io.Serializable;
  * @author fosa
  */
 public class EstacaoMeteorologica implements Serializable, Concept {
+    
+    private float taxaNeblina; //------------------------------
+    private float taxaFumaca;  // porcentagens de 0 a 100
+    private float taxaAves;    // obs: se pelo menos um animaal
+    private float taxaAnimais; //------------------------------
 
+    public float getTaxaNeblina() {
+        return taxaNeblina;
+    }
+
+    public void setTaxaNeblina(float taxaNeblina) {
+        this.taxaNeblina = taxaNeblina;
+    }
+
+    public float getTaxaFumaca() {
+        return taxaFumaca;
+    }
+
+    public void setTaxaFumaca(float taxaFumaca) {
+        this.taxaFumaca = taxaFumaca;
+    }
+
+    public float getTaxaAves() {
+        return taxaAves;
+    }
+
+    public void setTaxaAves(float taxaAves) {
+        this.taxaAves = taxaAves;
+    }
+
+    public float getTaxaAnimais() {
+        return taxaAnimais;
+    }
+
+    public void setTaxaAnimais(float taxaAnimais) {
+        this.taxaAnimais = taxaAnimais;
+    }
 }
