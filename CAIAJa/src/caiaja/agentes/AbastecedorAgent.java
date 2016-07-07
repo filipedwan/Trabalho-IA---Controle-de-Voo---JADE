0a6510e1d644a5de612c854e92a8824c0cb33164
Lucia <Lucia@Lucia-PC.paricarana.ufrr.br>
2016-07-07 19:59:51 -0300

    AbastecedorAgent

---
+++ CAIAJa/src/caiaja/agentes/AbastecedorAgent.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package caiaja.agentes;
+import jade.core.Agent;
+import caiaja.model.Abastecedor;
+
+/**
+ *
+ * @author Laian
+ */
+public class AbastecedorAgent extends Agent{
+
+    Abastecedor abastecedor;
+    
+    @Override
+    protected void setup() {
+        super.setup(); //To change body of generated methods, choose Tools | Templates.
+        
+        Object[] args = getArguments();
+        
+        
+        
+        
+    }
+    
+}
